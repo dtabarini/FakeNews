6f56455d0479896d275349bb8a05335b83251c99
anuvgupta <me@anuv.me>
2020-12-06 16:15:27 -0800

    update code, include preprocessing

--- app/server.py
+++ app/server.py
@@ -18,9 +18,17 @@ def json_response(obj, code = 200):
 # model
 @app.route('/model', methods=['POST'])
 def run_model():
-	title = request.form.get('title')
-	text = request.form.get('text')
 	try:
+		title = request.form.get('title')
+		text = request.form.get('text')
+		if text == "":
+			return json_response({
+				"status": False,
+				"data": {
+					"message": "missing parameter: text"
+				}
+			}, 400)
+		text = model.preprocess_text(text)
 		prediction = model.predict(title, text)
 		return json_response({
 			"status": True,
@@ -52,8 +60,8 @@ def index():
 
 # main
 print('FAKE NEWS CLASSIFIER')
-print('initializing model')
-model.initialize('data.csv')
+print('training model')
+model.train('data.csv')
 if __name__ == '__main__':
 	print('running flask')
 	app.run(threaded=True, port=8000)
